f6e8360b11daaf118430b20bc8dd3d11939f01d4
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-05 21:03:48 +0800

    build and package

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const native = require('./native/6')
+const binary = require('@mapbox/node-pre-gyp');
+const path = require('path');
+const binding_path = binary.find(path.resolve(path.join(__dirname, './package.json')));
+const native = require(binding_path);
 
 class CozoDb {
     constructor(path) {
