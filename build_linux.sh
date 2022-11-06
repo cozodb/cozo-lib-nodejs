95750efdf49b3b086da4b094b61369366d640987
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-06 15:46:07 +0800

    fix linux build

--- build_linux.sh
+++ build_linux.sh
@@ -3,5 +3,5 @@
 rm -fr native
 mkdir -p native/6
 cross build --target=x86_64-unknown-linux-gnu --release
-mv target/release/libcozo_node.so native/6/index.node
-yarn package
+mv target/x86_64-unknown-linux-gnu/release/libcozo_node.so native/6/index.node
+npm run package
